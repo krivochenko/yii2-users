00db7db794c23ebc2811e978f7b72eadd9c500d7
krivochenko <budyaga@gmail.com>
2015-06-17 14:49:50 +0300

    [new] - show default permissions on user view page

--- components/UserPermissionsWidget.php
+++ components/UserPermissionsWidget.php
@@ -2,8 +2,9 @@
 
 namespace budyaga\users\components;
 
-use budyaga\users\models\forms\LoginForm;
+use budyaga\users\models\AuthItem;
 use yii\base\Widget;
+use Yii;
 
 class UserPermissionsWidget extends Widget
 {
@@ -11,6 +12,7 @@ class UserPermissionsWidget extends Widget
 
     public function run()
     {
-        return $this->render('userPermissionsWidget', ['user' => $this->user]);
+        $defaultAssignments = AuthItem::find()->where(['in', 'name', Yii::$app->authManager->defaultRoles])->all();
+        return $this->render('userPermissionsWidget', ['user' => $this->user, 'defaultAssignments' => $defaultAssignments]);
     }
 }
--- components/views/userPermissionsWidget.php
+++ components/views/userPermissionsWidget.php
@@ -13,3 +13,19 @@ foreach ($user->assignments as $assignment) : ?>
     </div>
 </div>
 <?php endforeach;?>
+
+<?php foreach ($defaultAssignments as $assignment) : ?>
+<div class="list-group">
+    <div class="list-group-item list-group-item-info">
+        <h4 class="list-group-item-heading"><?= $assignment->description?></h4>
+        <?php
+        $children = $auth->getChildren($assignment->name);
+        if (count($children)) {
+            echo $this->context->render('_userPermissionsLevel', ['children' => $children]);
+        }
+        ?>
+    </div>
+</div>
+<?php endforeach;?>
+
+
